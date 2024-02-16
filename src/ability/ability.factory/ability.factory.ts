f64d6a10b2dcaca27be60d81c40ad6b040f22c5d
Gihan Shirantha <shirantha1999@gmail.com>
2024-02-16 14:09:57 +0530

    feat: implement the casl and add into Post req

---
+++ src/ability/ability.factory/ability.factory.ts
@@ -0,0 +1,39 @@
+import {
+  Ability,
+  AbilityBuilder,
+  AbilityClass,
+  ExtractSubjectType,
+  InferSubjects,
+} from '@casl/ability';
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+
+export enum Action {
+  Manage = 'mange',
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+export type Subjects = InferSubjects<typeof User> | 'all';
+export type AppAbility = Ability<[Action, Subjects]>;
+
+@Injectable()
+export class AbilityFactory {
+  defineAbility(user: User) {
+    const { can, cannot, build } = new AbilityBuilder(
+      Ability as AbilityClass<AppAbility>,
+    );
+    if (user.isAdmin) {
+      can(Action.Manage, 'all');
+    } else {
+      can(Action.Read, User);
+    }
+
+    return build({
+      detectSubjectType: (item) =>
+        item.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+}
